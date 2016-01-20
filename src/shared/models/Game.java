f14efeb53da912869d6ff7a4bbb31da0f5a3f702
Stephen Snyder <ssnyder2525@yahoo.com>
2016-01-19 22:05:28 -0700

    Filled in the methods I could think of in the Game class.

--- src/shared/models/Game.java
+++ src/shared/models/Game.java
@@ -15,20 +15,65 @@ public class Game {
 	CardDeck cardDeck = new CardDeck();
 	/**The players array contains four players that will be used by four clients.*/
 	ArrayList<Player> players = new ArrayList<Player>();
-	Player one = new Player();
-	Player two = new Player();
-	Player three = new Player();
-	Player four = new Player();
 	
 	/**
 	 * The Game class is a master class that contains all information having to do with a specific game
 	 * of settlers of catan.
 	 */
 	public Game() {
+		Player one = new Player();
+		Player two = new Player();
+		Player three = new Player();
+		Player four = new Player();
 		this.players.add(one);
 		this.players.add(two);
 		this.players.add(three);
 		this.players.add(four);
 	}
 
+	/**
+	 * Trades a player's resources for a new road on the map. It must border a settlement or road owned by the player.
+	 */
+	public void buildRoad(){}
+	
+	/**
+	 * Trades a player's resources for a new settlement on the map. The player must have a road leading to the spot wanted.
+	 * The selected place to build must also be at least two building spots away from any other settlement.
+	 */
+	public void buildSettlement(){}
+	
+	/**
+	 * Trades a player's resources for a new city on the map. The player must build it in place of an existing settlement.
+	 */
+	public void buildCity(){}
+	
+	/**
+	 * Trades a player's resources for a development card
+	 */
+	public void buyDevelopmentCard(){}
+	
+	/**
+	 * Allows a player to move the robber in exchange for a Soldier Card
+	 */
+	public void playSoldierCard(){}
+	
+	/**
+	 * Allows a player to build two roads in exchange for a Road Builder Card
+	 */
+	public void playRoadBuilderCard(){}
+	
+	/**
+	 * Allows a player to take all owned cards of a specified resource in exchange for a Monopoly Card
+	 */
+	public void playMonopolyCard(){}
+	
+	/**
+	 * Allows a player to choose two resources to be added to it's hand in exchange for a Year Of Plenty Card
+	 */
+	public void playYearOfPlentyCard(){}
+	
+	/**
+	 * Allows a player to offer an exchange of resources to one or more other players
+	 */
+	public void offerATrade(){}
 }
