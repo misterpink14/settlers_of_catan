c96e16d1156922c4427898ad98d8b67b168f7b3e
Stephen Snyder <ssnyder2525@yahoo.com>
2016-01-20 12:48:15 -0700

    Fixed the Proxy Interface

--- src/client/clientProxy/ProxyInterface.java
+++ src/client/clientProxy/ProxyInterface.java
@@ -7,7 +7,7 @@ import com.sun.org.glassfish.gmbal.ParameterNames;
  * @author Cody Burt
  *
  */
-public interface Proxy {
+public interface ProxyInterface {
 
 	/**
 	 * This function will call the server API at 
@@ -16,122 +16,66 @@ public interface Proxy {
 	 * @param JSON String of the server call parameters
 	 * @return JSON String
 	 */ 
-	public String postUserLogin() {
-		
-	}
+	public String postUserLogin();
 	
 	/**
 	 * This function will call the server API at
 	 * user slash register
 	 * @ParameterNames
 	 */
-	public String postUserRegister() {
-		
-	}
+	public String postUserRegister();
 	
-	public String getGamesList() {
-		
-	}
+	public String getGamesList();
 	
-	public String postGamesCreate() {
-		
-	}
+	public String postGamesCreate();
 	
-	public String postGamesJoin() {
-		
-	}
+	public String postGamesJoin();
 	
-	public String postGamesSave() {
-		
-	}
+	public String postGamesSave();
 	
-	public String postGamesLoad() {
-		
-	}
+	public String postGamesLoad();
 	
-	public String getGameModel() {
-		
-	}
+	public String getGameModel();
 	
-	public String postGameReset() {
-		
-	}
+	public String postGameReset();
 	
-	public String postGameCommands() {
-		
-	}
+	public String postGameCommands();
 	
-	public String getGameCommands() {
-		
-	}
+	public String getGameCommands();
 	
-	public String postMovesSendChat() {
-		
-	}
+	public String postMovesSendChat();
 	
-	public String postMovesRollNumber() {
-		
-	}
+	public String postMovesRollNumber();
 	
-	public String postMovesRobPlayer() {
-		
-	}
+	public String postMovesRobPlayer();
 	
-	public String postMovesFinishTurn() {
-		
-	}
+	public String postMovesFinishTurn();
 	
-	public String postMovesBuyDevCard() {
-		
-	}
+	public String postMovesBuyDevCard();
 	
-	public String postMovesYearOfPlenty() {
-		
-	}
+	public String postMovesYearOfPlenty();
 	
-	public String postMovesRoadBuilding() {
-		
-	}
+	public String postMovesRoadBuilding();
 	
-	public String postMovesSoldier() {
-		
-	}
+	public String postMovesSoldier();
 	
-	public String postMovesMonument() {
-		
-	}
+	public String postMovesMonument();
 	
-	public String postMovesBuildRoad() {
-		
-	}
+	public String postMovesBuildRoad();
 	
-	public String postMovesBuildCity() {
-		
-	}
+	public String postMovesBuildCity();
 	
-	public String postMovesBuildSettlement() {
-		
-	}
+	public String postMovesBuildSettlement();
 	
-	public String postMovesOfferTrade() {
-		
-	}
+	public String postMovesOfferTrade();
 	
-	public String postMovesAcceptTrade() {
-		
-	}
+	public String postMovesAcceptTrade();
 	
-	public String postMovesMaritimeTrade() {
-		
-	}
+	public String postMovesMaritimeTrade();
 	
-	public String postMovesDiscardCards() {
-		
-	}
+	public String postMovesDiscardCards();
 	
-	public String postUtilChangeLogLevel() {
-		
-	}
+	public String postUtilChangeLogLevel();
 	
 	
 	
