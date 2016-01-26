c423ae635417a4b4650b4282967ebcb23188deda
Stephen Snyder <ssnyder2525@yahoo.com>
2016-01-25 17:06:15 -0700

    Made first mapper class

--- src/shared/models/playerClasses/PlayerMapper.java
+++ src/shared/models/playerClasses/PlayerMapper.java
@@ -2,8 +2,19 @@ package shared.models.playerClasses;
 
 public class PlayerMapper {
 
-	public PlayerMapper() {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Replaces player classes using json input
+	 */
+	public PlayerMapper(String json) {
+		
+	}
+	
+	/**
+	 * Uses json to build new classes to replace the current model
+	 * @param json json object containing information to build new classes in the model
+	 */
+	public static void deserialize(String json) {
+		
 	}
 
 }
