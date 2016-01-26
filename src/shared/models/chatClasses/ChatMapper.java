b24ae6e8c763106927bb9c11834d89cb1f084881
Stephen Snyder <ssnyder2525@yahoo.com>
2016-01-25 17:08:56 -0700

    added mapper exception

--- src/shared/models/chatClasses/ChatMapper.java
+++ src/shared/models/chatClasses/ChatMapper.java
@@ -2,8 +2,20 @@ package shared.models.chatClasses;
 
 public class ChatMapper {
 
+	/**
+	 * Replaces chat classes using json input
+	 */
 	public ChatMapper() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * Uses json to build new classes to replace the current model
+	 * @param json json object containing information to build new classes in the model
+	 * @exception mapperException if the deserialization process goes wrong
+	 */
+	public static void deserialize(String json) {
+		
 	}
 
 }
--- src/shared/models/playerClasses/PlayerMapper.java
+++ src/shared/models/playerClasses/PlayerMapper.java
@@ -12,6 +12,7 @@ public class PlayerMapper {
 	/**
 	 * Uses json to build new classes to replace the current model
 	 * @param json json object containing information to build new classes in the model
+	 * @exception mapperException if the deserialization process goes wrong
 	 */
 	public static void deserialize(String json) {
 		
