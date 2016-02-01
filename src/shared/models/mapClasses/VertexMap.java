5c05b2dbe5a12649e45c02da0f7fbe74490e6048
Ben Thompson <misterpink14@gmail.com>
2016-02-01 14:18:17 -0700

    Implemented VertexMap

--- src/shared/models/mapClasses/VertexMap.java
+++ src/shared/models/mapClasses/VertexMap.java
@@ -2,6 +2,12 @@ package shared.models.mapClasses;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import shared.definitions.PieceType;
+import shared.locations.EdgeDirection;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexDirection;
 import shared.locations.VertexLocation;
 
 public class VertexMap 
@@ -10,48 +16,12 @@ public class VertexMap
 	
 	
 	
+// CONSTRUCTOR
 	public VertexMap() {}
+
 	
 	
-	/**
-	 * Check if a Vertex is occupied by a settlement or city
-	 * 
-	 * @param loc
-	 * @return
-	 * @throws IndexOutOfBoundsException
-	 */
-	public boolean isSettled(VertexLocation loc) throws IndexOutOfBoundsException
-	{
-		return true;
-	}
-	
-	
-	/**
-	 * Check if a Vertex is occupied by a settlement
-	 * 
-	 * @param loc
-	 * @return
-	 * @throws IndexOutOfBoundsException
-	 */
-	public boolean isSettlement(VertexLocation loc) throws IndexOutOfBoundsException
-	{
-		return true;
-	}
-	
-	
-	/**
-	 * Check if a Vertex is occupied by a settlement
-	 * 
-	 * @param loc
-	 * @return
-	 * @throws IndexOutOfBoundsException
-	 */
-	public boolean isCity(VertexLocation loc) throws IndexOutOfBoundsException
-	{
-		return true;
-	}
-	
-	
+// GETTER
 	/**
 	 * Get the Piece at a given VertexDirection
 	 * 
@@ -61,20 +31,140 @@ public class VertexMap
 	 */
 	public Piece getPiece(VertexLocation loc) throws IndexOutOfBoundsException
 	{
-		return Vertexes.get(loc);
+		if (!Vertexes.containsKey(loc.getNormalizedLocation()))
+		{
+			throw new IndexOutOfBoundsException();
+		}
+		return Vertexes.get(loc.getNormalizedLocation());
 	}
+
 	
 	
+// SETTER
 	/**
 	 * Set the Piece at the given VertexLocation
 	 * 
 	 * @param loc
 	 * @param p
+	 */
+	public void setVertex(VertexLocation loc, Piece p)
+	{
+		this.Vertexes.put(loc.getNormalizedLocation(), p);
+	}
+	
+	
+
+// METHODS
+	/**
+	 * Can do method for checking if a player can place a settlement
+	 * 
+	 * @param loc
+	 * @param ownerIndex
+	 * @return
 	 * @throws IndexOutOfBoundsException
 	 */
-	public void setVertex(VertexLocation loc, Piece p) throws IndexOutOfBoundsException
+	public boolean canPlaceCity(VertexLocation loc, int ownerIndex) throws IndexOutOfBoundsException
 	{
+		loc = loc.getNormalizedLocation();
+		if (!this.Vertexes.containsKey(loc))
+		{
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.Vertexes.get(loc) == null)
+		{
+			return false;
+		}
 		
+		Piece p = this.Vertexes.get(loc);
+		if (p.getOwner() == ownerIndex && p.getType() == PieceType.SETTLEMENT)
+		{
+			return true;
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Can do method for checking if a player can place a settlement
+	 * 
+	 * @param loc
+	 * @param ownerIndex
+	 * @return
+	 * @throws IndexOutOfBoundsException
+	 * @throws InvalidTypeException 
+	 */
+	public boolean canPlaceSettlement(VertexLocation loc, int ownerIndex) throws IndexOutOfBoundsException, InvalidTypeException
+	{
+		loc = loc.getNormalizedLocation();
+		if (!this.Vertexes.containsKey(loc))
+		{
+			throw new IndexOutOfBoundsException();
+		}
+		if (this.Vertexes.get(loc) == null)
+		{
+			return this._canPlaceSettlement(loc, ownerIndex);
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * Helper method for canPlaceSettlement. Checks vertexes that are 1 away from the given location. 
+	 *  
+	 * 
+	 * @param loc
+	 * @param ownerIndex
+	 * @return
+	 * @throws IndexOutOfBoundsException
+	 * @throws InvalidTypeException 
+	 */
+	private boolean _canPlaceSettlement(VertexLocation loc, int ownerIndex) throws IndexOutOfBoundsException, InvalidTypeException
+	{
+		loc = loc.getNormalizedLocation();
+		switch(loc.getDir())
+		{
+			case NorthEast:
+			{
+				if (!this.checkLocation(VertexDirection.NorthWest, loc.getHexLoc()) && 
+						!this.checkLocation(VertexDirection.East, loc.getHexLoc()) &&
+						!this.checkLocation(VertexDirection.NorthWest, loc.getHexLoc().getNeighborLoc(EdgeDirection.NorthEast)))
+				{
+					return false;
+				}
+				break;
+			}
+			case NorthWest:
+			{
+				if (!this.checkLocation(VertexDirection.West, loc.getHexLoc()) && 
+						!this.checkLocation(VertexDirection.NorthEast, loc.getHexLoc()) &&
+						!this.checkLocation(VertexDirection.NorthWest, loc.getHexLoc().getNeighborLoc(EdgeDirection.NorthWest)))
+				{
+					return false;
+				}
+				break;
+			}
+			default:
+				throw new InvalidTypeException();
+		}
+		return true;
+	}
+	
+	
+	/**
+	 * Check's if the given location
+	 * 
+	 * @param dir
+	 * @param hexLoc
+	 * @return
+	 */
+	private boolean checkLocation(VertexDirection dir, HexLocation hexLoc)
+	{
+		VertexLocation new_loc = (new VertexLocation(hexLoc, dir)).getNormalizedLocation();
+		if (this.Vertexes.containsKey(new_loc) && this.Vertexes.get(new_loc) != null)
+		{
+			return false;
+		}
+		return true;
 	}
 	
 }
