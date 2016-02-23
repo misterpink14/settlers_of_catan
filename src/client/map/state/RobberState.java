15ea7ad48cea9a1f9e54c4c5bc395dd5265342b4
Ben Thompson <misterpink14@gmail.com>
2016-02-22 21:52:03 -0700

    Added State classes

---
+++ src/client/map/state/RobberState.java
@@ -0,0 +1,95 @@
+package client.map.state;
+
+import client.data.RobPlayerInfo;
+import shared.definitions.PieceType;
+import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
+import shared.locations.VertexLocation;
+
+public class RobberState implements MapStateInterface {
+
+	@Override
+	public void initFromModel() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean canPlaceRoad(EdgeLocation edgeLoc) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceSettlement(VertexLocation vertLoc) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceCity(VertexLocation vertLoc) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean canPlaceRobber(HexLocation hexLoc) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void placeRoad(EdgeLocation edgeLoc) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void placeSettlement(VertexLocation vertLoc) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void placeCity(VertexLocation vertLoc) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void placeRobber(HexLocation hexLoc) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startMove(PieceType pieceType, boolean isFree, boolean allowDisconnected) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cancelMove() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playSoldierCard() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playRoadBuildingCard() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void robPlayer(RobPlayerInfo victim) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
