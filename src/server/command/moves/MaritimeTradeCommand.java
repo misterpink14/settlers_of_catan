8f72df1342afdf86e87922a94d6812f27200df8a
Bo Pace <bopace@gmail.com>
2016-03-23 16:22:24 -0600

    MaritimeTradeCommand implemented

--- src/server/command/moves/MaritimeTradeCommand.java
+++ src/server/command/moves/MaritimeTradeCommand.java
@@ -3,17 +3,24 @@ package server.command.moves;
 
 import java.util.Map;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import server.ServerException;
 import server.command.ACommand;
 import server.facade.IServerFacade;
+import shared.communication.proxy.MaritimeTrade;
+import shared.definitions.ResourceType;
 
 /**
  * Command for maritime trade
  * 	Server end-point: /moves/maritimeTrade POST
  * 
- * @author benthompson
+ * @author benthompson & Bo Pace
  */
 public class MaritimeTradeCommand extends ACommand {
+	
+	MaritimeTrade maritimeTrade;
 
 	/**
 	 * {
@@ -31,24 +38,56 @@ public class MaritimeTradeCommand extends ACommand {
 	 */
 	public MaritimeTradeCommand(Map<String, String> cookies, IServerFacade facade, String jsonBody) throws ServerException {
 		super(cookies.get("catan.user"), facade, Integer.parseInt(cookies.get("catan.game"))); 
-		// TODO parse the jsonBody
+		
+		JsonObject json = new JsonParser().parse(jsonBody).getAsJsonObject();
+		ResourceType inputResource = ResourceType.BRICK;
+		switch(json.get("inputResource").getAsString()) {
+		case "Brick":
+			inputResource = ResourceType.BRICK;
+			break;
+		case "Ore":
+			inputResource = ResourceType.ORE;
+			break;
+		case "Sheep":
+			inputResource = ResourceType.SHEEP;
+			break;
+		case "Wheat":
+			inputResource = ResourceType.WHEAT;
+			break;
+		case "Wood":
+			inputResource = ResourceType.WOOD;
+			break;
+		}
+		ResourceType outputResource = ResourceType.BRICK;
+		switch(json.get("outputResource").getAsString()) {
+		case "Brick":
+			outputResource = ResourceType.BRICK;
+			break;
+		case "Ore":
+			outputResource = ResourceType.ORE;
+			break;
+		case "Sheep":
+			outputResource = ResourceType.SHEEP;
+			break;
+		case "Wheat":
+			outputResource = ResourceType.WHEAT;
+			break;
+		case "Wood":
+			outputResource = ResourceType.WOOD;
+			break;
+		}
+		
+		maritimeTrade = new MaritimeTrade(
+			inputResource,
+			outputResource,
+			json.get("ratio").getAsInt(),
+			json.get("playerIndex").getAsInt()
+		);
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public String getResponse() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getCookie() {
-		// TODO Auto-generated method stub
-		return null;
+		this.response = this.getFacade().maritimeTrade(maritimeTrade, this.getGameID());
 	}
 
 }
