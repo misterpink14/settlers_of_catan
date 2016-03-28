3b208fc436de10946dca9e7d7c6a087b749c164c
Ben Thompson <misterpink14@gmail.com>
2016-03-27 23:34:35 -0600

    Fixed warning

--- src/client/map/state/Setup2State.java
+++ src/client/map/state/Setup2State.java
@@ -1,18 +1,6 @@
 package client.map.state;
 
-import java.util.ArrayList;
-
-import client.clientFacade.ClientFacade;
 import client.map.IMapView;
-import shared.definitions.HexType;
-import shared.definitions.PortType;
-import shared.locations.EdgeDirection;
-import shared.locations.EdgeLocation;
-import shared.locations.HexLocation;
-import shared.locations.VertexDirection;
-import shared.locations.VertexLocation;
-import shared.models.mapClasses.Piece;
-import shared.models.mapClasses.WaterHex;
 
 /**
  * This is the last phase of the Setup. Players take turns in reverse
