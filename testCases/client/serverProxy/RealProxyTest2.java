1057edd739d736beb3d666470464bf23a63ed957
Cody Burt <cody.burt.21@gmail.com>
2016-03-30 16:40:58 -0600

    Tests for real server commands

--- testCases/client/serverProxy/RealProxyTest2.java
+++ testCases/client/serverProxy/RealProxyTest2.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import shared.communication.proxy.CreateGameRequestParams;
-import shared.communication.proxy.Credentials;
-import shared.communication.proxy.JoinGameRequestParams;
+import shared.communication.proxy.*;
 
 public class RealProxyTest2 {
 
@@ -58,6 +56,11 @@ public class RealProxyTest2 {
 		}
 		catch (Exception e) {
 			//Setup is only needed for the initial setup of the server
+			credentials.username = "personOne";
+			credentials.password = "personOne";
+			assertEquals(this.realProxy.loginUser(credentials), "Success");
+			params.color = "blue";
+			this.realProxy.joinGame(params);
 			//fail("Exception");
 		}
 		
@@ -90,6 +93,67 @@ public class RealProxyTest2 {
 			success = response.equals("Success");
 			assertTrue(success);
 
+		} catch (Exception e) {
+			fail("shouldn't get an exception");
+		}
+	}
+	
+	@Test
+	public void testListGames() {
+		try {
+			String response = realProxy.getGamesList();
+			Boolean success = response.length() > 0;
+			assertTrue(success);
+
+		} catch (Exception e) {
+			fail("shouldn't get an exception");
+		}
+	}
+	
+	@Test
+	public void testCreateGame() {
+		try {
+			String response = realProxy.createGame(new CreateGameRequestParams("testCreate", true, true, true));
+			Boolean success = response.length() > 0;
+			assertTrue(success);
+
+		} catch (Exception e) {
+			fail("shouldn't get an exception");
+		}
+	}
+	
+	@Test
+	public void testGetGameModel() {
+		try {
+			
+			String response = realProxy.getGameModel(-1);
+			Boolean success = response.length() > 0;
+			assertTrue(success);
+
+		} catch (Exception e) {
+			fail("shouldn't get an exception");
+		}
+	}
+	
+	@Test
+	public void testBuildRoad() {
+		try {
+			String response = realProxy.buildRoad(new BuildRoad(0, true, 0, 0, "NW"));
+			Boolean success = response.length() > 0;
+			assertTrue(success);
+
+		} catch (Exception e) {
+			fail("shouldn't get an exception");
+		}
+	}
+	
+	@Test
+	public void testBuildSettlement() {
+		try {
+			String response = realProxy.buildSettlement(new BuildSettlement(0, true, 0, 0, "NW"));
+			Boolean success = response.length() > 0;
+			assertTrue(success);
+			
 		} catch (Exception e) {
 			fail("shouldn't get an exception");
 		}
