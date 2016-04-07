e78b7b76c2809624772d0c715a1a361cc2a69dc1
skydrennan <skydrennan@gmail.com>
2016-04-06 20:08:36 -0600

    Untested, but I think SQLGameDAO should be done

--- src/server/database/dao/SqlGameDAO.java
+++ src/server/database/dao/SqlGameDAO.java
@@ -13,6 +13,7 @@ import server.database.DatabaseException;
 import server.database.DatabaseRepresentation;
 import shared.models.Game;
 import shared.serializerJSON.Deserializer;
+import shared.serializerJSON.Serializer;
 
 /**
  * 
@@ -28,15 +29,88 @@ public class SqlGameDAO implements IGameDAO {
 	}
 
 	@Override
-	public Game getGame(int gameID) {
-		// TODO Auto-generated method stub
-		return null;
+	public Game getGame(int gameID) throws DatabaseException {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String query = "SELECT gameID, title, gameJSON FROM Games WHERE gameID = ?";
+			stmt = db.getConnection().prepareStatement(query);
+			stmt.setInt(1, gameID);
+			
+			rs = stmt.executeQuery();
+			if(rs.next()) {
+				int id = rs.getInt(1);
+				String title = rs.getString(2);
+				String gameJSON = rs.getString(3);
+				Game g = new Game();
+				g.setId(id);
+				g.setTitle(title);
+				JsonObject jsonObj = new JsonParser().parse(gameJSON).getAsJsonObject();
+				Deserializer.getInstance().deserialize(g, jsonObj);
+				return g;
+			} else {
+				throw new DatabaseException("Could not find game with id " + gameID);
+			}
+		} catch (SQLException e) {
+			throw new DatabaseException("Could not get game with id " + gameID, e);
+		} finally {
+			DatabaseRepresentation.safeClose(rs);
+			DatabaseRepresentation.safeClose(stmt);
+		}
+	}
+	
+	public boolean gameInDB(int gameID) throws DatabaseException {
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String query = "SELECT count(*) FROM Games WHERE gameID = ?";
+			stmt = db.getConnection().prepareStatement(query);
+			stmt.setInt(1, gameID);
+			
+			rs = stmt.executeQuery();
+			if(rs.next() && rs.getInt(1) == 1) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (SQLException e) {
+			throw new DatabaseException("Could not get game with id " + gameID, e);
+		} finally {
+			DatabaseRepresentation.safeClose(rs);
+			DatabaseRepresentation.safeClose(stmt);
+		}
 	}
 
 	@Override
-	public void saveGame(Game game) {
-		// TODO Auto-generated method stub
+	public void saveGame(Game game) throws DatabaseException {
+		PreparedStatement stmt = null;
 		
+		try {
+			String query = "";
+			String gameJSON = Serializer.getInstance().serialize(game);
+			if (gameInDB(game.getId())) {
+				query = "UPDATE Games SET gameJSON = ? WHERE gameID = ?";
+				stmt = db.getConnection().prepareStatement(query);
+				stmt.setString(1, gameJSON);
+				stmt.setInt(2, game.getId());
+			} else {
+				query = "INSERT INTO Games (gameID, title, gameJSON) values (?, ?, ?)";
+				stmt = db.getConnection().prepareStatement(query);
+				stmt.setInt(1, game.getId());
+				stmt.setString(2, game.getTitle());
+				stmt.setString(3, gameJSON);
+			}
+			
+			if (stmt.executeUpdate() != 1) {
+				throw new DatabaseException("Could not update game");
+			}
+		} catch (SQLException e) {
+			throw new DatabaseException("Could not update game", e);
+		} finally {
+			DatabaseRepresentation.safeClose(stmt);
+		}
 	}
 
 	public List<Game> getAllGames() throws DatabaseException {
